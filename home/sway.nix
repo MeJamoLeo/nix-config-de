b1cba4d5ed4db0c3847d32642b2dcd71be01a0be
MeJamoLeo <55238651+MeJamoLeo@users.noreply.github.com>
2024-08-15 18:28:46 -0500

    update

--- home/sway.nix
+++ home/sway.nix
@@ -22,7 +22,7 @@ in
 				};
 			};
 			output = {
-				"*".bg = "./wallpaper fill";
+				"*".bg = "../wallpaper/river-and-mountains.jpg fill";
 			};
 			bars = [ { position = "top"; } ];
 			window.titlebar = false;
