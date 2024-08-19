bf1a917d595e9c6c234ea4dba63c660048315cdb
MeJamoLeo <55238651+MeJamoLeo@users.noreply.github.com>
2024-08-19 10:13:26 -0500

    add laptop

--- flake.nix
+++ flake.nix
@@ -25,9 +25,25 @@
 		hyprland = {
 			url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 		};
+
 		hyprland-plugins = {
 			url = "github:hyprwm/hyprland-plugins";
 			inputs.hyprland.follows = "hyprland";
+		};
+
+		hy3 = {
+			url = "github:outfoxxed/hy3";
+			inputs.hyprland.follows = "hyprland";
+		};
+
+		Hyprspace = {
+			url = "github:KZDKM/Hyprspace";
+			inputs.hyprland.follows = "hyprland";
+		};
+
+		split-monitor-workspaces = {
+			url = "github:Duckonaut/split-monitor-workspaces";
+			inputs.hyprland.follows = "hyprland"; # <- make sure this line is present for the plugin to work as intended
 		};
 	};
 
@@ -45,6 +61,33 @@
 				system = "x86_64-linux";
 				modules = [
 					./hosts/suika
+#___________________Home_Manager
+					home-manager.nixosModules.home-manager
+					{
+						home-manager = {
+							useGlobalPkgs = true;
+							useUserPackages = true;
+							extraSpecialArgs.inputs = inputs;
+							users.treo = import ./home;
+						};
+						nixpkgs.config.allowUnfree = true;
+					}
+#___________________COSMIC
+					{
+						nix.settings = {
+							substituters = [ "https://cosmic.cachix.org/" ];
+							trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+						};
+					}
+					nixos-cosmic.nixosModules.default
+				];
+				specialArgs = {inherit inputs;};
+			};
+
+			"budou" = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./hosts/budou
 #___________________Home_Manager
 					home-manager.nixosModules.home-manager
 					{
